063279fc20d6d814997f38bfdde6c23f5c8e1627
Rakesh Sagar <Developer@mail.com>
2024-08-31 20:26:45 +0530

    Added first page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import './App.css';
 import Form from '@rjsf/core';
-import { STOCK_FORM, STOCK_FORM_UI } from './formconfig/formdata';
+import { INITIAL_FORM, INITIAL_FORM_UI } from './formconfig/formdata';
 import validator from '@rjsf/validator-ajv8';
 
 const App = () => {
   const [formData, setFormData] = React.useState(null);
   return (
     <div className='container'>
-    <Form
-      uiSchema={STOCK_FORM_UI}
-      schema={STOCK_FORM}
-      formData={formData}
-      onChange={(e) => setFormData(e.formData)}
-      validator={validator}
-    />
+      <Form
+        uiSchema={INITIAL_FORM_UI}
+        schema={INITIAL_FORM}
+        formData={formData}
+        onChange={(e) => setFormData(e.formData)}
+        validator={validator}
+      >
+        <div className='row justify-content-end'>
+            <button className='submit col-3 btn' type="submit">Submit</button>
+        </div>
+      </Form>
     </div>
   );
 };
--- src/formconfig/formdata.js
+++ src/formconfig/formdata.js
@@ -1,84 +1,81 @@
-export const STOCK_FORM = {
-    "title": "A registration form",
-    "description": "A simple form example.",
+export const INITIAL_FORM = {
+    "title": "Stock Purchase Check List",
+    "description": "This is a list of questions to know answers to before purchase the stock",
     "type": "object",
     "required": [
-      "firstName",
+      "stockName",
       "lastName"
     ],
     "properties": {
-      "firstName": {
+      "stockName": {
         "type": "string",
-        "title": "First name",
-        "default": "Chuck"
+        "title": "What is the stock name?",
       },
-      "lastName": {
-        "type": "string",
-        "title": "Last name"
-      },
-      "age": {
-        "type": "integer",
-        "title": "Age"
-      },
-      "bio": {
-        "type": "string",
-        "title": "Bio"
-      },
-      "password": {
-        "type": "string",
-        "title": "Password",
-        "minLength": 3
-      },
-      "telephone": {
-        "type": "string",
-        "title": "Telephone",
-        "minLength": 10
-      },
-      "destination": {
-        "type": "string",
-        "title": "Where are you headed?",
-        "enum": [
-          "canada",
-          "usa",
-          "international"
-        ],
-        "enumNames": [
-          "Canada",
-          "USA"
-        ]
-      }
+      // "lastName": {
+      //   "type": "string",
+      //   "title": "Last name"
+      // },
+      // "age": {
+      //   "type": "integer",
+      //   "title": "Age"
+      // },
+      // "bio": {
+      //   "type": "string",
+      //   "title": "Bio"
+      // },
+      // "password": {
+      //   "type": "string",
+      //   "title": "Password",
+      //   "minLength": 3
+      // },
+      // "telephone": {
+      //   "type": "string",
+      //   "title": "Telephone",
+      //   "minLength": 10
+      // },
+      // "destination": {
+      //   "type": "string",
+      //   "title": "Where are you headed?",
+      //   "enum": [
+      //     "canada",
+      //     "usa",
+      //     "international"
+      //   ],
+      //   "enumNames": [
+      //     "Canada",
+      //     "USA"
+      //   ]
+      // }
     }
   };
 
-export const STOCK_FORM_UI = {
-    "firstName": {
+export const INITIAL_FORM_UI = {
+    "stockName": {
       "ui:autofocus": true,
       "ui:emptyValue": "",
-      "ui:placeholder": "ui:emptyValue causes this field to always be valid despite being required",
-      "ui:autocomplete": "family-name",
+      "ui:placeholder": "Name of the stock",
       "ui:enableMarkdownInDescription": true,
-      "ui:description": "Make text **bold** or *italic*. Take a look at other options [here](https://markdown-to-jsx.quantizor.dev/)."
     },
-    "lastName": {
-      "ui:autocomplete": "given-name",
-      "ui:enableMarkdownInDescription": true,
-      "ui:description": "Make things **bold** or *italic*. Embed snippets of `code`. <small>And this is a small texts.</small> "
-    },
-    "age": {
-      "ui:widget": "updown",
-      "ui:title": "Age of person",
-      "ui:description": "(earth year)"
-    },
-    "bio": {
-      "ui:widget": "textarea"
-    },
-    "password": {
-      "ui:widget": "password",
-      "ui:help": "Hint: Make it strong!"
-    },
-    "telephone": {
-      "ui:options": {
-        "inputType": "tel"
-      }
-    }
+    // "lastName": {
+    //   "ui:autocomplete": "given-name",
+    //   "ui:enableMarkdownInDescription": true,
+    //   "ui:description": "Make things **bold** or *italic*. Embed snippets of `code`. <small>And this is a small texts.</small> "
+    // },
+    // "age": {
+    //   "ui:widget": "updown",
+    //   "ui:title": "Age of person",
+    //   "ui:description": "(earth year)"
+    // },
+    // "bio": {
+    //   "ui:widget": "textarea"
+    // },
+    // "password": {
+    //   "ui:widget": "password",
+    //   "ui:help": "Hint: Make it strong!"
+    // },
+    // "telephone": {
+    //   "ui:options": {
+    //     "inputType": "tel"
+    //   }
+    // }
   };
